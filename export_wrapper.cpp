86606e964968ae05ee116bd6e7e09b25637ca0b7
Neural VFX <38269894+NeuralVFX@users.noreply.github.com>
2020-08-24 15:19:20 +0200

    Update export_wrapper.cpp

--- export_wrapper.cpp
+++ export_wrapper.cpp
@@ -1,13 +1,6 @@
 #include "export_wrapper.h"
 #include "vision.h"
 
-#include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-
-#include "torch/torch.h"
-#include <torch/script.h>
-
 
 vision vision_util = vision();
 
